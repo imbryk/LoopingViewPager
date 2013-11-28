b98ee400eb112a1c40c6982bbc30ff53ce3dd1eb
naghtarr <naghtarr@gmail.com>
2013-11-28 12:09:45 +0700

    Fixed zero division exception in case no data in adapter

--- src/com/imbryk/viewPager/LoopPagerAdapterWrapper.java
+++ src/com/imbryk/viewPager/LoopPagerAdapterWrapper.java
@@ -54,7 +54,8 @@ public class LoopPagerAdapterWrapper extends PagerAdapter {
 
     int toRealPosition(int position) {
         int realCount = getRealCount();
-
+        if (realCount == 0)
+            return 0;
         int realPosition = (position-1) % realCount;
         if (realPosition < 0)
             realPosition += realCount;
